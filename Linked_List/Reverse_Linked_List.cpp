de2346cb070150b68e7d740fb248f5cfd028bf9a
Hardik <145928869+HardikJangra@users.noreply.github.com>
2026-05-02 22:28:46 +0530

    Add iterative reversal of singly linked list
    Implemented an iterative solution to reverse a singly linked list using three pointers.

--- Linked_List/Reverse_Linked_List.cpp
+++ Linked_List/Reverse_Linked_List.cpp
@@ -1,1 +1,62 @@
+/*
+Problem: Reverse Linked List
+Platform: LeetCode
+Pattern: Linked List (Iterative Reversal)
+Folder: 05_Linked_List
+Difficulty: Easy
 
+Problem Statement:
+Given the head of a singly linked list, reverse the list
+and return the new head.
+
+Approach:
+1. Use three pointers:
+   - prev → previous node
+   - curr → current node
+   - next → next node
+2. Reverse link:
+   curr->next = prev
+3. Move all pointers forward.
+
+Time Complexity: O(n)
+Space Complexity: O(1)
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* curr = head;
+        ListNode* prev = NULL;
+
+        while(curr != NULL) {
+            ListNode* next = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        return prev;
+    }
+};
+
+/*
+Key Learning:
+1. Classic 3-pointer technique.
+2. Reverse pointers one by one.
+3. In-place reversal (O(1) space).
+4. Base for many linked list problems.
+*/
